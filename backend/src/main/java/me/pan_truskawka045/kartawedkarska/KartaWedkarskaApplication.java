88352045013c99a83c05df2fcd9f28c3ef981ef2
PanTruskawka045 <pan.truskawka045@gmail.com>
2024-04-15 02:36:40 +0200

    Initial Commit

---
+++ backend/src/main/java/me/pan_truskawka045/kartawedkarska/KartaWedkarskaApplication.java
@@ -0,0 +1,13 @@
+package me.pan_truskawka045.kartawedkarska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartaWedkarskaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartaWedkarskaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/me/pan_truskawka045/kartawedkarska/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pan_truskawka045.kartawedkarska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
