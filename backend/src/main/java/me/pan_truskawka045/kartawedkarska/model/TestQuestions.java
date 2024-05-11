7ef3a49eca2d8668cd8ec77c1e49a569dd2911fd
PanTruskawka045 <pan.truskawka045@gmail.com>
2024-05-11 17:12:28 +0200

    fix test question constructor (Kacper)

--- backend/src/main/java/me/pan_truskawka045/kartawedkarska/model/TestQuestions.java
+++ backend/src/main/java/me/pan_truskawka045/kartawedkarska/model/TestQuestions.java
@@ -10,7 +10,6 @@ public class TestQuestions {
 
     private TestQuestion[] questions;
 
-    @AllArgsConstructor
     @Getter
     @Setter
     public static final class TestQuestion {
@@ -18,6 +17,11 @@ public class TestQuestions {
         private int id;
         private int markedAnswer = -1;
         private int answersMap; //first two bits maps correct answer location, next two bits marks second (first wrong) answer location, etc.
+
+        public TestQuestion(int id, int answersMap) {
+            this.id = id;
+            this.answersMap = answersMap;
+        }
 
     }
 
